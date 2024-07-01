42f7b87d8c61d8084ec57f1c9997eb8a4a3afc2f
Upenderm8030 <167904523+Upenderm8030@users.noreply.github.com>
2024-07-01 14:58:25 +0530

    apis moved
    apis moved

--- application/models/User_model.php
+++ application/models/User_model.php
@@ -5,16 +5,51 @@ class User_model extends CI_Model {
         $this->load->database();
     }
 
+
+    private function generate_unique_id() {
+        // Get the last inserted user ID
+        $this->db->select('uniq_id');
+        $this->db->order_by('uniq_id', 'DESC');
+        $this->db->limit(1);
+        $query = $this->db->get('users');
+        $last_user = $query->row();
+
+        if ($last_user) {
+            // Extract the number from the last user ID
+            $last_id_number = (int) substr($last_user->uniq_id, 2);
+            // Increment the number by 1
+            $new_id_number = $last_id_number + 1;
+        } else {
+            // If there are no users yet, start with 1
+            $new_id_number = 1;
+        }
+
+        // Determine the number of digits in the new ID number
+        $num_digits = strlen((string) $new_id_number);
+        // Calculate the number of leading zeros required
+        $total_length = max(6, $num_digits);
+        $num_leading_zeros = $total_length - $num_digits;
+
+        // Format the new ID with leading zeros and the prefix 'WH'
+        $new_id = 'WH' . str_pad($new_id_number, $total_length, '0', STR_PAD_LEFT);
+
+        return $new_id;
+    }
+
     public function register($data) {
+      
         $this->db->select("*");
         $this->db->from('users');
         $this->db->where('email', $data['email']);
         $query = $this->db->get();
         if($query->row_array()){
+           
             return false;
         }else{
             $token = bin2hex(random_bytes(50)); // Generate a token
             $data['token']=$token;
+            $data['uniq_id'] = $this->generate_unique_id();
+           
             return $this->db->insert('users', $data);
         }
     }
@@ -57,7 +92,18 @@ class User_model extends CI_Model {
 
     // Method to fetch rooms with pagination
      public function get_rooms($limit, $offset) {
+        $this->db->where('isActive_users', 1);
+        $query = $this->db->get('rooms');
+       
+        return $query->result_array();
+    }
+
+    public function adminget_rooms($limit, $offset) {
+        //$this->db->where('isActive', 1);
+        // $this->db->order_by('roomId','desc');
+        $this->db->order_by('latter_datetime','desc');
         $query = $this->db->get('rooms', $limit, $offset);
+       
         return $query->result_array();
     }
 
@@ -88,7 +134,7 @@ class User_model extends CI_Model {
     }
 
     public function update_room_status($id, $status) {
-        $data = array('isActive' => $status);
+        $data = array('isActive_users' => $status);
         $this->db->where('id', $id);
         return $this->db->update('rooms', $data);
     }
@@ -104,6 +150,7 @@ class User_model extends CI_Model {
         $this->db->where('startTime >=', $startTime);
         $this->db->where('endDate <=', $endDate);
         $this->db->where('endTime <=', $endTime);
+        // $this->db->where('isActive', 1);
         $query = $this->db->get('room');
         return $query->result();
     }
@@ -243,8 +290,58 @@ class User_model extends CI_Model {
         return $query->result();
     }
 
+    public function addedpercheck($data = array()) {
     
+        $this->db->where('user_id', $data['user_id']);
+        $this->db->where('room_id', $data['room_id']);
+        $query = $this->db->get('rooms_userlist');
+        return $query->result_array();
+    }
 
+    
+public function getuserdetails($user_id) {
+        
+        $this->db->where('id', $user_id);
+        $query = $this->db->get('users');
+        $user = $query->row();
+    
+        if ($user) {
+            return $user;
+        } else {
+            return false;
+        }
+    }
+
+
+    public function postwinnersdata($data = array()){
+
+        return $this->db->insert("winners_new_list", $data);
+    }
+
+    public function getwinnersdata($room_id) {
+        
+     
+
+        $this->db->where('room_id', $room_id);
+        $this->db->order_by('tot_amount_send','asc');
+        $query = $this->db->get('winners_new_list');
+        return $query->result_array();
+    
+       
+    }
+
+    public function update_room_bothstatus($room_id) {
+        $data = array('isActive_users' => 0,'isActive' => 0);
+        $this->db->where('roomId', $room_id);
+        return $this->db->update('rooms', $data);
+    }
+
+    public function updateroomdetails($roomdata) {
+         //print_r($data);die;
+        $data = array('winningAmount' => $roomdata['winningAmount'],'totalParticipants' => $roomdata['totalParticipants']);
+        $this->db->where('roomId', $roomdata['roomId']);
+        return $this->db->update('rooms', $data);
+    }
     
    
 
